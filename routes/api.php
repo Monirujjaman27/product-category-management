a906189db3ee4fec56577b49f2a6c6032971091b
Monirujjaman27 <mdmunirujjaman045@gmail.com>
2025-01-07 02:31:19 +0600

    update readme.md file

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
 
 // Route By api Resources
 Route::apiResources(['category'  => CategoryController::class]); // 👉 :: category api ::
