707effe265a9dbf8ea28413231c5f7d062f6fabf
Monirujjaman27 <mdmunirujjaman045@gmail.com>
2025-04-20 16:23:23 +0600

    add pwa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,11 @@
 import "./bootstrap";
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function (registration) {
+        console.log(`SW registered successfully!`);
+    }).catch(function (registrationError) {
+        console.log(`SW registration failed`);
+    });
+}
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router"; // Import Vue Router
